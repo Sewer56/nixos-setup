2d4420095b2c36c613db08dd06e7f1f2bd0955a7
Sewer56 <sewer56lol@googlemail.com>
2025-07-20 23:41:51 +0100

    Changed: Moved appearance settings to theme.nix for hyprland

--- users/sewer/home-manager/desktop/hyprland/settings.nix
+++ users/sewer/home-manager/desktop/hyprland/settings.nix
@@ -2,45 +2,8 @@
   wayland.windowManager.hyprland.settings = {
     # General settings
     general = {
-      gaps_in = 5;
-      gaps_out = 0;
-      border_size = 2;
-      "col.active_border" = "$accent $accent2 45deg";
-      "col.inactive_border" = "$inactive";
       layout = "dwindle";
       allow_tearing = false;
-    };
-
-    # Decoration settings
-    decoration = {
-      rounding = 10;
-      blur = {
-        enabled = true;
-        size = 3;
-        passes = 1;
-      };
-      shadow = {
-        enabled = true;
-        range = 4;
-        render_power = 3;
-        color = "rgba(1a1a1aee)";
-      };
-    };
-
-    # Animations
-    animations = {
-      enabled = true;
-      bezier = [
-        "myBezier, 0.05, 0.9, 0.1, 1.05"
-      ];
-      animation = [
-        "windows, 1, 7, myBezier"
-        "windowsOut, 1, 7, default, popin 80%"
-        "border, 1, 10, default"
-        "borderangle, 1, 8, default"
-        "fade, 1, 7, default"
-        "workspaces, 1, 6, default"
-      ];
     };
 
     # Layout settings
--- users/sewer/home-manager/desktop/hyprland/theme.nix
+++ users/sewer/home-manager/desktop/hyprland/theme.nix
@@ -5,6 +5,47 @@
     "$accent2" = "rgb(00ff99)";
     "$inactive" = "rgb(595959)";
 
+    # General appearance settings
+    general = {
+      gaps_in = 5;
+      gaps_out = 0;
+      border_size = 2;
+      "col.active_border" = "$accent $accent2 45deg";
+      "col.inactive_border" = "$inactive";
+    };
+
+    # Decoration settings
+    decoration = {
+      rounding = 10;
+      blur = {
+        enabled = true;
+        size = 3;
+        passes = 1;
+      };
+      shadow = {
+        enabled = true;
+        range = 4;
+        render_power = 3;
+        color = "rgba(1a1a1aee)";
+      };
+    };
+
+    # Animations
+    animations = {
+      enabled = true;
+      bezier = [
+        "myBezier, 0.05, 0.9, 0.1, 1.05"
+      ];
+      animation = [
+        "windows, 1, 7, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+      ];
+    };
+
     # Window transparency rules
     windowrulev2 = [
       # Terminal transparency
