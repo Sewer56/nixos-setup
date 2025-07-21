d181482c5ba08016606a2559c74436e1ab4ce7a3
Sewer56 <sewer56lol@googlemail.com>
2025-07-21 12:10:27 +0100

    Refactor: Rofi colour styles to use same reference colour.

--- users/sewer/home-manager/desktop/hyprland/programs/rofi/themes/default.nix
+++ users/sewer/home-manager/desktop/hyprland/programs/rofi/themes/default.nix
@@ -2,9 +2,15 @@
   # Generate shared theme files
   home.file.".config/rofi/themes/shared/colors.rasi".text = ''
     * {
-        background-alt: #282839FF;
-        foreground:     #D9E0EEFF;
-        selected:       #7AA2F7FF;
+        background-alt:      #282839FF;
+        foreground:          #D9E0EEFF;
+        selected:            #7AA2F7FF;
+        background-dark:     black / 60%;
+        background-medium:   black / 40%;
+        background-light:    black / 10%;
+        border-subtle:       white / 25%;
+        background-input:    white / 5%;
+        background-selected: white / 15%;
     }
   '';
 
@@ -26,5 +32,4 @@
     source = ./clipboard;
     recursive = true;
   };
-
 }
