d02a3de53cfe338e5782e48b4744b6c2199d8a48
Sewer56 <sewer56lol@googlemail.com>
2025-08-02 03:49:40 +0100

    Changed: Use proportional font for workspace switcher. to get right centering.

--- users/sewer/home-manager/desktop/hyprland/programs/waybar/modules/workspaces.nix
+++ users/sewer/home-manager/desktop/hyprland/programs/waybar/modules/workspaces.nix
@@ -43,6 +43,7 @@ semantic: {
       color: ${semantic.border};
       background: transparent;
       border: 1.5px solid transparent;
+      font-family: "JetBrains Mono Nerd Font Propo";
       font-size: 10pt;
       padding-left: 2pt;
       padding-right: 2pt;
