b2c2e6137e63bf0598b9b62baca767ff6dcddf81
Sewer56 <sewer56lol@googlemail.com>
2025-08-05 08:43:19 +0100

    Changed: Don't use tmpfs for temp, as that kills huge rebuilds.

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -1,7 +1,10 @@
 {...}: {
   # Configure tmpfs for /tmp directory
-  boot.tmp = {
-    useTmpfs = true;
-    tmpfsSize = "50%";
-  };
+  boot.tmp.cleanOnBoot = true;
+
+  # Don't use tmpfs, because huge builds can fail due to size limits
+  #boot.tmp = {
+  #  useTmpfs = true;
+  #  tmpfsSize = "50%";
+  #};
 }
