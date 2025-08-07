f1208963ad9f950513694572d1213a6419ab3742
Sewer56 <sewer56lol@googlemail.com>
2025-08-07 07:41:08 +0100

    Added: CUD/Nvidia Support to btop.

--- users/sewer/home-manager/programs/monitor.nix
+++ users/sewer/home-manager/programs/monitor.nix
@@ -1,6 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   programs.btop = {
     enable = true;
+    package = pkgs.btop.override {
+      cudaSupport = true;
+    };
     settings = {
       update_ms = 500;
     };
