6cc6920d3e7d33ed14717f68f786a84e0b1d9c8c
Sewer56 <sewer56lol@googlemail.com>
2025-08-10 10:28:22 +0100

    Added: Suspend/Resume for Nvidia Desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running 'nixos-help').
-{pkgs, ...}: {
+{pkgs, lib, ...}: {
   imports = [
     # Include the results of the hardware scan.
     # nixos-generate-config --root /mnt
@@ -40,6 +40,9 @@
   services.xserver.videoDrivers = [
     "nvidia"
   ];
+
+  # Desktop-specific Nvidia power management override for suspend/resume
+  hardware.nvidia.powerManagement.enable = lib.mkForce true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
