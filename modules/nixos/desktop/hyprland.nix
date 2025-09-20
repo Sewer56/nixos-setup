32b6ce8611fec91990a1d38819ec4fd1e5c5db37
Sewer56 <sewer56lol@googlemail.com>
2025-09-20 11:59:47 +0100

    Updated: Packages, switched to upstream Hyprland, Locking nixpkgs due to bugs

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   # Hyprland Desktop
   programs.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     withUWSM = true; # Session management
     xwayland.enable = true;
   };
