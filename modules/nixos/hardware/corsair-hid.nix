f1a3ff78f9b487a6773c396667d163cdcb8e2b2c
Sewer56 <sewer56lol@googlemail.com>
2025-09-22 13:33:56 +0100

    Changed: Workaround breaking bug in nixpkgs https://github.com/NixOS/nixpkgs/issues/444209

--- modules/nixos/hardware/corsair-hid.nix
+++ modules/nixos/hardware/corsair-hid.nix
@@ -1,7 +1,13 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
-  hardware.ckb-next.enable = lib.mkIf config.hostOptions.hardware.corsair.enable true;
+  hardware.ckb-next = {
+    enable = lib.mkIf config.hostOptions.hardware.corsair.enable true;
+    package = pkgs.ckb-next.overrideAttrs (old: {
+      cmakeFlags = (old.cmakeFlags or []) ++ ["-DUSE_DBUS_MENU=0"];
+    });
+  };
 }
