a8d51d9dcf2f3c393e3335f7e8b492aa24fd690f
Sewer56 <sewer56lol@googlemail.com>
2025-12-03 00:38:35 +0000

    Improve: Move the generated thumbnails instead of copy

--- packages/thumbnailers/tumbler-dds-thumbnailer/dds-thumbnailer.sh
+++ packages/thumbnailers/tumbler-dds-thumbnailer/dds-thumbnailer.sh
@@ -9,16 +9,10 @@ SIZE="$1"
 INPUT_FILE="$2"
 OUTPUT_FILE="$3"
 
-# Cleanup function
-cleanup() {
-  [[ -n "${TMP_FILE:-}" && -f "$TMP_FILE" ]] && rm -f "$TMP_FILE"
-}
-trap cleanup EXIT
-
 # Use temp file with .png extension - ImageMagick needs the extension to detect output format
 # (Tumbler's %o output path has no extension)
 if TMP_FILE="$(mktemp --tmpdir tumbler-dds-XXXXXX.png)"; then
-  convert -thumbnail "${SIZE}x${SIZE}" "$INPUT_FILE" "$TMP_FILE" && cp "$TMP_FILE" "$OUTPUT_FILE"
+  convert -thumbnail "${SIZE}x${SIZE}" "$INPUT_FILE" "$TMP_FILE" && mv "$TMP_FILE" "$OUTPUT_FILE"
 else
   exit 1
 fi
