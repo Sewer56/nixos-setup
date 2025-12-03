a0c9cd047f346acab001b4bd3fd2cd638f7c6bdf
Sewer56 <sewer56lol@googlemail.com>
2025-12-03 00:38:35 +0000

    Improve: Move the generated thumbnails instead of copy

--- packages/thumbnailers/tumbler-text-thumbnailer/text-thumbnailer.sh
+++ packages/thumbnailers/tumbler-text-thumbnailer/text-thumbnailer.sh
@@ -11,12 +11,6 @@ SIZE="$1"
 INPUT_FILE_NAME="$2"
 OUTPUT_FILE_NAME="$3"
 
-# Cleanup function
-cleanup() {
-  [[ -n "${TMP_FILE:-}" && -f "$TMP_FILE" ]] && rm -f "$TMP_FILE"
-}
-trap cleanup EXIT
-
 POINTSIZE=$((SIZE / 8))
 [[ $POINTSIZE -lt 4 ]] && POINTSIZE=4
 
@@ -31,7 +25,7 @@ if TMP_FILE="$(mktemp --tmpdir tumbler-text-XXXXXX.png)"; then
     -border 1x1 -bordercolor "#00aaff" \
     -font "$FONT_PATH" -pointsize "$POINTSIZE" \
     label:"$CHOPPED_FILE_CONTENT" \
-    "$TMP_FILE" && cp "$TMP_FILE" "$OUTPUT_FILE_NAME"
+    "$TMP_FILE" && mv "$TMP_FILE" "$OUTPUT_FILE_NAME"
 else
   exit 1
 fi
