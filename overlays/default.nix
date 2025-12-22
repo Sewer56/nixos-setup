cddaca5977e67db1c4d7683a99539c9a0569a9d1
Sewer56 <sewer56lol@googlemail.com>
2025-12-22 03:33:00 +0000

    Added: Rust demangling for heaptrack

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,22 @@
 final: prev: {
+  # Override heaptrack with rustc-demangle support for Rust symbol demangling
+  # rustc_demangle is a runtime dependency loaded via dlopen()
+  # - Qt apps (heaptrack_gui, heaptrack_print) are wrapped via qtWrapperArgs
+  # - heaptrack_interpret (non-Qt) must be wrapped separately in postFixup
+  heaptrack = prev.heaptrack.overrideAttrs (oldAttrs: {
+    qtWrapperArgs =
+      (oldAttrs.qtWrapperArgs or [])
+      ++ [
+        "--prefix LD_LIBRARY_PATH : ${prev.rustc-demangle}/lib"
+      ];
+    postFixup =
+      (oldAttrs.postFixup or "")
+      + ''
+        wrapProgram $out/lib/heaptrack/libexec/heaptrack_interpret \
+          --prefix LD_LIBRARY_PATH : ${prev.rustc-demangle}/lib
+      '';
+  });
+
   # Override magnetic-catppuccin-gtk with our updated local package
   magnetic-catppuccin-gtk = final.callPackage ../packages/catppuccin-gtk {
     inherit (prev) lib stdenv fetchFromGitHub jdupes sassc gtk-engine-murrine;
