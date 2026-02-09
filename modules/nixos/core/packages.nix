ef4470834c305e811038aca311bfc5c237bcfd2e
Sewer56 <sewer56lol@googlemail.com>
2026-02-09 06:38:16 +0000

    Changed: Use 1st party .NET to work around an issue
    https://github.com/bagetter/BaGetter/issues/264

--- modules/nixos/core/packages.nix
+++ modules/nixos/core/packages.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: let
+  # Using binary SDKs instead of source-built to ensure Windows-specific assemblies (like IIS) are available
   dotnet-combined = (with pkgs.dotnetCorePackages;
     combinePackages [
-      sdk_10_0
-      sdk_9_0
-      sdk_8_0
+      sdk_10_0-bin
+      sdk_9_0-bin
+      sdk_8_0-bin
     ]).overrideAttrs (finalAttrs: previousAttrs: {
     # This is needed to install workload in $HOME
     # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
