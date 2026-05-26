216f9a394e72a738198cb7a525333fe78efed224
Sewer56 <sewer56lol@googlemail.com>
2026-05-26 15:29:51 +0100

    Fixed: portals on hyprland

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -12,6 +12,14 @@
     withUWSM = true; # Session management
   };
 
+  # Strip cap_sys_nice from Hyprland wrapper.
+  # The NixOS module sets cap_setpcap,cap_sys_nice=ep for real-time scheduling,
+  # but this makes every Hyprland child inherit CAP_SYS_NICE in ambient set.
+  # xdg-desktop-portal then can't open /proc/<child-pid>/root (kernel blocks
+  # access when caller caps aren't subset of portal caps), breaking ALL portals
+  # (FileChooser, Screenshot, etc). See flatpak/xdg-desktop-portal#1691.
+  security.wrappers.Hyprland.capabilities = lib.mkForce "";
+
   # Must be synced with home-manager module, due to home-manager bug that overrides.
   xdg.portal = {
     enable = lib.mkForce true;
