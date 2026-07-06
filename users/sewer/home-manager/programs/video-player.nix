cedb4c79d36eee9539d3b7344b32a0cca5540792
Sewer56 <admin@sewer56.dev>
2026-07-06 11:37:47 +0100

    mpv: work around borked streams

--- users/sewer/home-manager/programs/video-player.nix
+++ users/sewer/home-manager/programs/video-player.nix
@@ -3,6 +3,8 @@
     enable = true;
     config = {
       osd-status-msg = "\${playback-time/full} / \${duration} (\${percent-pos}%)\\nframe: \${estimated-frame-number} / \${estimated-frame-count}";
+      vd-lavc-skiploopfilter = "all"; # always skip deblock on corrupt streams
+      vd-lavc-show-all = "yes";
     };
   };
 }
